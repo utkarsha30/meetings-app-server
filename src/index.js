8694d4131507e651fa7ffacf148718e1f0313fed
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2023-02-21 21:11:06 +0530

    add updation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 require("dotenv/config");
 const express = require("express");
-var bodyParser = require("body-parser");
+const cors = require("cors");
 // connecting to database
 const { connect } = require("./db/init");
 
 // create an application object
 const app = express();
+//to avoid cors policy error
+app.use(cors({ origin: "*" }));
 //for request body data
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send(
+    '<div style="width:200px; margin: auto auto;"><img width="100%"  src="https://media.tenor.com/2jd3xi2WVt0AAAAC/recurring-settings.gif"></div><div style="width:220px; margin: 0 auto;"><h2>Server is Running...</h2></div>'
+  );
+});
 //routes
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/users", require("./routes/users.route"));
